3aa79727a15d8df8e268fc36692fe51c60e46a87
mykhailopulov <mihail.pulov.247@gmail.com>
2026-01-20 00:33:15 +0200

    BankAccount class 1task requirements completed

--- src/main/java/Practice_2/BankAccount.java
+++ src/main/java/Practice_2/BankAccount.java
@@ -1,4 +1,34 @@
 package Practice_2;
 
 public class BankAccount {
+    private final String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+
+        // Check if balance is not negative
+        if (initialBalance < 0) {
+            System.out.println("Invalid initial balance. Must be greater than zero.");
+            this.balance = 0;
+        }
+        else {
+            this.balance = initialBalance;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        // Check if amount is not negative
+        if (amount < 0) {
+            System.out.println("Invalid amount. Must be greater than zero.");
+        }
+        else {
+            this.balance += amount;
+            System.out.println("Deposited " + amount + " to balance.");
+        }
+    }
 }
